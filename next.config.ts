110b4babbd1fa32c8a2a95f1dd55b67ffeabce89
Rammah Karpous <rammahkarpous@outlook.com>
2026-01-06 14:02:50 +0100

    chore: change configuration to deploy the app on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // If your repository name is not 'username.github.io', uncomment and set the basePath
-  // basePath: '/omnient-bord-game',
-  // trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
