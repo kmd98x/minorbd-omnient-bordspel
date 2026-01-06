1d1657c951bd7848223e9de70f46b13895903904
Rammah Karpous <rammahkarpous@outlook.com>
2026-01-06 13:54:02 +0100

    feat: add deploy script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repository name is not 'username.github.io', uncomment and set the basePath
+  // basePath: '/omnient-bord-game',
+  // trailingSlash: true,
 };
 
 export default nextConfig;
